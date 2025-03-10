1f0f095cf40016c2d8ca4d9f4204726a6eddc91b
Apoorva Kumari <96675938+apoorva0-5@users.noreply.github.com>
2025-03-10 12:41:30 +0530

    Update api.js

--- api.js
+++ api.js
@@ -20,7 +20,7 @@ Wiki
 Security
 Insights
 sxp-mapi-backend-onboarding/build/reports/tests/test/js/report.js
-/1/egv/*
+/1/egv/
 /api/2/wallet/egv/active
 /api/2/wallet/egv/link
 /1/egv/balance
@@ -28,11 +28,11 @@ sxp-mapi-backend-onboarding/build/reports/tests/test/js/report.js
 /1/egv/location
 /1/exchange/tnc
 /3/checkout/(pgResponse|pgCancelResponse)
-/3/checkout/*
+/3/checkout/
 /api/5/checkout/contact
 /3/checkout/paymentToken
-/1/payzippy/*
-/(1|2)/plutus/*
+/1/payzippy/
+/(1|2)/plutus/
 /1/cards/saved/account-form
 /1/fintech/apply/now 
 /1/fintech/fetch-metadata
